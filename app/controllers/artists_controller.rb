5616f8ef4256578afcd2b34a35c9163e4cde81da
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-10 20:46:28 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,22 @@
 class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
